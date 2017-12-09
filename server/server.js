321b0cd6f3f81ab5e9d85c09c0aaa5cd9c2e9115
ggcadc <ggcadc@gmail.com>
2017-12-08 16:57:17 -0800

    refactored to single component and have more work to do

--- server/server.js
+++ server/server.js
@@ -2,14 +2,12 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8000;
 
 app.use(express.static(__dirname + '/public'));
 
-function onConnection(socket){
+io.on('connection', socket => {
   socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
-}
-
-io.on('connection', onConnection);
+});
 
 http.listen(port, () => console.log(`listening on ${port}`));
